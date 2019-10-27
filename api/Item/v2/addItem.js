e88e8f396c0c52e16812977fdab092f5c6976162
Sam MacGregor <sammacgregor59@gmail.com>
2019-10-27 22:19:00 +1000

    fixed data model issue with item api

--- api/Item/v2/addItem.js
+++ api/Item/v2/addItem.js
@@ -9,7 +9,7 @@ exports.addItem = function (req, res) {
   var newItem = new Item(req.body);
 
   //handles null error 
-  if (!newItem.Summary || !newItem.ItemTypeID || !newItem.ItemStatusID || !newItem.ItemPriorityID) {
+  if (!newItem.summary || !newItem.item_type_id || !newItem.item_status_id || !newItem.item_priority_id) {
     console.log(JSON.stringify(req.body));
     res.status(400).send({ error: true, message: 'Mandatory attributes have not been provided' });
 
@@ -21,22 +21,22 @@ exports.addItem = function (req, res) {
 
     sql
       .query("INSERT INTO Item.Item (Item_Type_ID, Item_Status_ID, Item_Priority_ID, Team_ID, Board_ID, Reporter_ID, Assignee_ID, Sprint_ID, Summary, Description, Created_Date, Created_By, Updated_Date, Updated_By) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING Item_ID", [
-      newItem.ItemTypeID,
-      newItem.ItemStatusID,
-      newItem.ItemPriorityID,
-      newItem.TeamID,
-      newItem.BoardID,
-      newItem.ReporterID,
-      newItem.AssigneeID,
-      newItem.SprintID,
-      newItem.Summary,
-      newItem.Description,
-      newItem.CreatedDate,
-      newItem.CreatedBy,
-      newItem.UpdatedDate,
-      newItem.UpdatedBy
+      newItem.item_type_id,
+      newItem.item_status_id,
+      newItem.item_priority_id,
+      newItem.team_id,
+      newItem.board_id,
+      newItem.reporter_id,
+      newItem.assignee_id,
+      newItem.sprint_id,
+      newItem.summary,
+      newItem.description,
+      newItem.created_date,
+      newItem.created_by,
+      newItem.updated_date,
+      newItem.updated_by
       ]).then(result => {
-        newItem.ItemID = JSON.stringify(result.rows[0].item_id);
+        newItem.item_id = JSON.stringify(result.rows[0].item_id);
 
         return res.send({ error: false, data: newItem, message: 'addItem' })
 
--- api/Item/v2/appModel.js
+++ api/Item/v2/appModel.js
@@ -2,23 +2,25 @@
 
 //Item object constructor
 var Item = function (item) {
-    this.ItemID = item.ItemID;
-    this.ItemTypeID = item.ItemTypeID;
-    this.ItemStatusID = item.ItemStatusID;
-    this.ItemPriorityID = item.ItemPriorityID;
-    this.TeamID = item.TeamID;
-    this.BoardID = item.BoardID;
-    this.ReporterID = item.ReporterID;
-    this.AssigneeID = item.AssigneeID;
-    this.SprintID = item.SprintID;
-    this.Summary = item.Summary;
-    this.Description = item.Description;
-    this.CreatedDate = item.CreatedDate;
-    this.CreatedBy = item.CreatedBy;
-    this.UpdatedDate = item.UpdatedDate;
-    this.UpdatedBy = item.UpdatedBy;
+    this.item_id = item.item_id;
+    this.item_type_id = item.item_type_id;
+    this.item_priority_id = item.item_priority_id;
+    this.item_status_id = item.item_status_id;
+    this.sprint_id = item.sprint_id;
+    this.board_id = item.board_id;
+    this.assignee_id = item.assignee_id;
+    this.reporter_id = item.reporter_id;
+    this.summary = item.summary;
+    this.description = item.description;
+    this.team_id = item.team_id;
+    this.created_by = item.created_by;
+    this.created_date = item.created_date;
+    this.updated_by = item.updated_by;
+    this.updated_date = item.updated_date;
 
 
 };
 
 module.exports = Item;
+
+
