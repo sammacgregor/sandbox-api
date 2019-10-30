ecf31775917b3d10570e8196eb316fc5c361d9bd
Sam MacGregor <sammacgregor59@gmail.com>
2019-10-30 19:00:23 +1000

    testing

--- api/Board/v2/deleteBoard.js
+++ api/Board/v2/deleteBoard.js
@@ -5,24 +5,44 @@ var sql = require('../../db.js');
 
 
 exports.deleteBoard = function (req, res) {
-  let BoardID = req.params.BoardID;
+    let BoardID = req.params.BoardID;
 
-  if (!BoardID) {
-    return res.status(400).send({ error: true, message: 'Please provide BoardID' });
-}
+    if (!BoardID) {
+        return res.status(400).send({ error: true, message: 'Please provide BoardID' });
+    }
 
 
+    var unassociateItemsresult = sql
+        .query("UPDATE Item.Item SET board_id = null, sprint_id = null WHERE board_id = $1", [BoardID])
+        .then(result => {
+            var outcome = { error: false, message: "unassociateItemsToBoard" }
+            return outcome;
 
-sql
-.query("DELETE FROM Board.Board WHERE Board_ID = $1", [BoardID])
-.then(result => {
-    return res.send({ error: false, data: result.rows, message: 'deleteBoard' })
+        })
+        .catch(e => {
+            console.error(e.stack)
+            var outcome = { error: true, message: "unassociateItemsToBoard" }
+            return outcome;
+        })
 
-})
-.catch(e => {
-    console.error(e.stack)
-    res.send({ error: true, data: e.stack, message: 'deleteBoard' })
-})
+
+        
+
+    sql
+        .query("DELETE FROM Board.Board WHERE Board_ID = $1", [BoardID])
+        .then(result => {
+
+            if (unassociateItemsresult.error) {
+                return res.send({ error: true, data: result.rows, message: 'board deleted but items could not be unassociated' })
+
+            }
+            return res.send({ error: false, data: result.rows, message: 'deleteBoard' })
+
+        })
+        .catch(e => {
+            console.error(e.stack)
+            res.send({ error: true, data: e.stack, message: 'deleteBoard' })
+        })
 
 
 };
