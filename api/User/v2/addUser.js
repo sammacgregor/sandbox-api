7575a0924044f5e5a8114979b837b5b55317ff5c
Sam MacGregor <sammacgregor59@gmail.com>
2019-11-09 11:47:48 +1000

    fixing auth

--- api/User/v2/addUser.js
+++ api/User/v2/addUser.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 var User = require('./appModel.js');
 
@@ -20,33 +18,31 @@ exports.addUser = function (req, res) {
   else {
 
 
-    bcrypt.hash(newUser.password, saltRounds, function (err, hash) {
 
-      
 
-      sql
-        .query("INSERT INTO System.User (Password, Given_Name, Surname, Email, Mobile, Created_Date, Created_By, Updated_Date, Updated_By) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING user_id", [
-          hash,
-          newUser.given_name,
-          newUser.surname,
-          newUser.email,
-          newUser.mobile,
-          newUser.created_date,
-          newUser.created_by,
-          newUser.updated_date,
-          newUser.updated_by
-        ]).then(result => {
-          newUser.user_id = JSON.stringify(result.rows[0].user_id);
+    sql
+      .query("INSERT INTO System.User (Password, Given_Name, Surname, Email, Mobile, Created_Date, Created_By, Updated_Date, Updated_By) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING user_id", [
+        hash,
+        newUser.given_name,
+        newUser.surname,
+        newUser.email,
+        newUser.mobile,
+        newUser.created_date,
+        newUser.created_by,
+        newUser.updated_date,
+        newUser.updated_by
+      ]).then(result => {
+        newUser.user_id = JSON.stringify(result.rows[0].user_id);
 
-          return res.send({ error: false, data: newUser, message: 'addUser' })
+        return res.send({ error: false, data: newUser, message: 'addUser' })
 
-        })
-        .catch(e => {
-          console.error(e.stack)
-          res.send({ error: true, data: e.stack, message: 'addUser' })
-        })
+      })
+      .catch(e => {
+        console.error(e.stack)
+        res.send({ error: true, data: e.stack, message: 'addUser' })
+      })
 
-    });
+
 
 
 
